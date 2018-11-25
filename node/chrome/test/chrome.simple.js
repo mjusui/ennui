b3b34bf34160146a405b395a275fb96819ccc584
mjusui <mjusui@gmail.com>
2018-11-25 16:56:28 +0000

    prepare dom test

---
+++ node/chrome/test/chrome.simple.js
@@ -0,0 +1,23 @@
+enn=require('../lib/ennui.js');
+const Chrome=require('chromy');
+
+enn.asrt((test,end,cmnt)=>{
+  cmnt('chrome.simple');
+
+  const chrome=new Chrome();
+
+  chrome.chain()
+  .goto('http://masquerade.ninja')
+  .evaluate(()=>{
+     return document
+     .getElementsByTagName(
+       'title'
+     )[0].textContent;
+  }).result((tit)=>{
+    test(tit==='Outernet Service - Papillon');
+  }).end()
+  .then(()=>{
+    chrome.close();
+    end();
+  });
+});
